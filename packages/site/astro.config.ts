0dec19ed8c92e5c696dbf1142d0a06dc032dbf74
Mathieu KIM ROBIN <mthrobin@gmail.com>
2026-08-22 15:14:55 +0200

    various

--- packages/site/astro.config.ts
+++ packages/site/astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://elupedia.fr',
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
